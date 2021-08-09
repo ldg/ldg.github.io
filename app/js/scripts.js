a8539c2866fae6bafd0f1d6f7ed6ebc531391266
smldgweb <sales.ldgwebdesign.com>
2021-08-08 20:25:09 -0400

    dropdown menu updates

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,21 +1,11 @@
+//hamburger menu
 let mobileMenu = document.querySelector('.mobile-menu-toggle');
 let menuContainer = document.querySelector('.mobile-menu-container');
-
-// submenus
-let submenuTrigger = document.querySelector('.carat');
-let subMenuEl = document.querySelector('.submenu');
-
-console.log(subMenuEl);
 
 function hideMenu() {
     menuContainer.classList.add('hide');
 }
 hideMenu();
-
-// function hideSubMenu() {
-//     subMenuEl.style.display = "none";
-// }
-// hideSubMenu();
 
 mobileMenu.addEventListener('click', function(){
 
@@ -24,8 +14,44 @@ mobileMenu.addEventListener('click', function(){
 
 });
 
-submenuTrigger.addEventListener('click', function(){
-    subMenuEl.classList.toggle('submenu-show');
-    subMenuEl.classList.toggle('submenu-hide');
-});
+// submenus
+let submenuTrigger = document.getElementsByClassName('carat');
+let subMenuEl = document.getElementsByClassName('submenu');
+
+console.log(subMenuEl);
+// add hide class to submenu
+function hideSubMenu() {
+    for(let i = 0;i < subMenuEl.length; i++ ){
+        subMenuEl[i].classList.add('submenu-hide');
+    }
+}
+hideSubMenu();
+
+//add event listener to first submenuTrigger
+// submenuTrigger[0].addEventListener('click', function(){
+//     subMenuEl[0].classList.toggle('submenu-show');
+//     subMenuEl[0].classList.toggle('submenu-hide'); 
+// })
+
+
+
+function submenuToggle(){
+    for(let i = 0; i < submenuTrigger.length; i++ ) {
+
+        submenuTrigger[i].addEventListener('click', function(){
+            subMenuEl[i].classList.toggle('submenu-show');
+            subMenuEl[i].classList.toggle('submenu-hide'); 
+            console.log(i);
+        })
+        console.log(i);
+            
+    }
+    console.log('submenuToggle triggered'); 
+}
+submenuToggle();
+
+// submenuTrigger.addEventListener('click', function(){
+//     subMenuEl.classList.toggle('submenu-show');
+//     subMenuEl.classList.toggle('submenu-hide');
+// });
 
