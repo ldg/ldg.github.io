1de9dd055dae05dacb8018108696cec1933bd1a9
smldgweb <sales.ldgwebdesign.com>
2021-08-15 13:25:05 -0400

    leaving menu javascript unfinished for the moment

--- app/js/menu.js
+++ app/js/menu.js
@@ -2,28 +2,20 @@ let mobileMenuToggle = document.querySelector('.mobile-menu-toggle'),
     mobileMenuContainer = document.querySelector('.mobile-menu-container'),
     hasSubmenu = document.querySelectorAll('.has-submenu'),
     subMenu = document.querySelectorAll('.submenu'),
+    menu = subMenu.nextElementSibling,
     items = document.querySelectorAll('.item');
 
 mobileMenuToggle.addEventListener('click', function(){
     mobileMenuContainer.classList.toggle('active');
-    console.log('clicked');
 });
 
 function submenuToggle (){
-    
-    // let submenu = this.nextElementSibling;
-    // submenu.classList.toggle('active');
-    if(this.nextElementSibling.classList.contains('active')){
-        console.log('this contains active')
-    } else {
-        console.log('no active here!');
-    }
+    let submenu = this.nextElementSibling;
+    submenu.classList.toggle('active');
 }
 
 for( let item of items  ) {
-
- item.addEventListener('click', submenuToggle, false );  
-        
+    item.addEventListener('click', submenuToggle, false );     
 }
 
 
