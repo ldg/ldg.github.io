1770141faad71fbc717d5407ce50612ce0f6a102
smldgweb <sales.ldgwebdesign.com>
2021-08-17 20:36:28 -0400

    disabled close all submenu function

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,22 +1,41 @@
-let mobileMenuToggle = document.querySelector('.mobile-menu-toggle'),
-    mobileMenuContainer = document.querySelector('.mobile-menu-container'),
-    hasSubmenu = document.querySelectorAll('.has-submenu'),
-    subMenu = document.querySelectorAll('.submenu'),
-    menu = subMenu.nextElementSibling,
-    items = document.querySelectorAll('.item');
+/* Variables */
+const mobileMenuToggle = document.querySelector('.mobile-menu-toggle');
+const mobileMenuContainer = document.querySelector('.mobile-menu-container');
+const hasSubmenu = document.querySelector('.has-submenu');
+const subMenu = document.querySelectorAll('.submenu');
+const items = document.querySelectorAll('.item');
 
+/* add evenet listener and function to toggle Mobile menu */
 mobileMenuToggle.addEventListener('click', function(){
     mobileMenuContainer.classList.toggle('active');
 });
 
+
+/* toggle sub-menus */
 function submenuToggle (){
     let submenu = this.nextElementSibling;
     submenu.classList.toggle('active');
 }
 
+/* add event listeners to submenus */
 for( let item of items  ) {
     item.addEventListener('click', submenuToggle, false );     
 }
 
 
+/* Close Submenu From Anywhere */
+// function closeSubmenu(e) {
+   
+//     let submenus = mobileMenuContainer.querySelectorAll('.submenu');
 
+//     submenus.forEach( function(submenu){
+//         if(submenu.classList.contains('active') ){
+//             submenu.classList.remove('active');
+//         }
+//     })
+    
+
+//     console.log('in the mobile menu container');
+
+//   }
+// mobileMenuContainer.addEventListener('click', closeSubmenu, false);
