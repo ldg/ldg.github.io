9ab9e57786a1720bc50744c5d3e73d47489f0972
smldgweb <sales.ldgwebdesign.com>
2021-08-22 17:00:19 -0400

    simplified mobile menus

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,18 +1,6 @@
 const hamburger = document.getElementById('mobilemenu-toggle');
 const mobileContainer = document.getElementById('mobilemenu-container');
 
-// set mobile menu default to hidden
-// function setMobileMenuDefault(){
-//     let w = window.innerWidth;
-//     if( w <= 1024){
-//         mobileContainer.classList.add('hide');
-//     } else {
-//         mobileContainer.classList.remove('hide');
-//     }
-//     console.log(w);
-// }
-// setMobileMenuDefault();
-
 //toggle show/hide for mobile menu 
 function toggleMobileMenu() {
     mobileContainer.classList.toggle('show'); 
