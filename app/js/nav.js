d4b9b84941d1b0ebc934f796bebbcaf453d76750
smldgweb <sales.ldgwebdesign.com>
2021-08-23 17:37:56 -0400

    added animation to hamburger menu,updated body styles on mobile

--- app/js/nav.js
+++ app/js/nav.js
@@ -2,6 +2,14 @@ const hamburger = document.getElementById('mobilemenu-toggle');
 const mobileContainer = document.getElementById('mobilemenu-container');
 //toggle show/hide for mobile menu 
 function toggleMobileMenu() {
+    let hamburger1 = document.querySelector( '.hamburger:nth-child(1)' );
+    let hamburger2 = document.querySelector( '.hamburger:nth-child(2)' );
+    let hamburger3 = document.querySelector( '.hamburger:nth-child(3)' );
+
+    hamburger1.classList.toggle('rotate-right');
+    hamburger2.classList.toggle('no-opacity');
+    hamburger3.classList.toggle('rotate-left');
+
     mobileContainer.classList.toggle('show'); 
 }
 //event listener for mobile menu toggle
