cd4bf04056309e3f2ec7b33e3501909710dffce6
smldgweb <sales.ldgwebdesign.com>
2021-08-22 20:25:06 -0400

    removed hidden characters from nav.js

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,10 +1,8 @@
 const hamburger = document.getElementById('mobilemenu-toggle');
 const mobileContainer = document.getElementById('mobilemenu-container');
-
 //toggle show/hide for mobile menu 
 function toggleMobileMenu() {
     mobileContainer.classList.toggle('show'); 
 }
-
 //event listener for mobile menu toggle
 hamburger.addEventListener('click', toggleMobileMenu, false);
